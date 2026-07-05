ce819cc53b4c88fbab67e1aaff0c10165843c9a0
Alexander Tverskoy <astverskoy@gmail.com>
2026-07-05 23:35:14 +0300

    Initializing the Dark Mode support package

---
+++ darkmode.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit darkmode;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  uDarkStyle, uImport, uWin32WidgetSetDark;
+
+implementation
+
+end.
